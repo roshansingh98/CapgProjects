e4dcce40fad22b6ae949c315db405afe1b2f14de
Roshan Singh <singhroshan98@gmail.com>
2020-11-02 00:55:41 +0530

    SpringBootJPA added

---
+++ CustomerBootJPA/src/test/java/com/cg/customerappboot/CustomerBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.customerappboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
